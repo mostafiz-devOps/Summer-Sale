092a8ae47a72cb37b01e618553e762697e24cad5
mostafiz-devOps <mostafizurofficial@outlook.com>
2025-09-02 22:07:25 +0600

    Order Summury update

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+
+let orderCount = 1;
+const cards = document.querySelectorAll('.card');
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('click', function () {
+        const productName = card.querySelector('h3').innerText;
+
+        const productPrice = card.querySelector('p').innerTex;
+
+        const orderContainer = document.getElementById('order-summary');
+        const p = document.createElement('p');
+        p.innerText = orderCount + ". " + productName;
+        orderContainer.appendChild(p);
+        orderCount++;
+
+    })
+}
