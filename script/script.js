51c8ec13cf268e8423c039328706703bf48dee87
masud-k <contact.masud99@gmail.com>
2025-09-03 23:58:25 +0600

    Total Price calculate

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
 
 let orderCount = 1;
+let totalPrice = 0;
 const cards = document.querySelectorAll('.card');
 for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
     card.addEventListener('click', function () {
-        const productName = card.querySelector('h3').innerText;
+        const productName = card.querySelector("h3").innerText;
 
-        const productPrice = card.querySelector('p').innerTex;
+        const productPrice = parseFloat(card.querySelector("h4").innerText.split(" ")[0]);
+        console.log(productPrice);
 
         const orderContainer = document.getElementById('order-summary');
         const p = document.createElement('p');
@@ -14,5 +16,7 @@ for (let i = 0; i < cards.length; i++) {
         orderContainer.appendChild(p);
         orderCount++;
 
+        totalPrice += productPrice;
+        document.getElementById('total-price').innerText = totalPrice.toFixed(2) + " TK";
     })
 }
